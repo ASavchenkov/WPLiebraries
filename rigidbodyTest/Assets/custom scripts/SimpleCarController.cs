a9c379f90152ef0b1f187bc5d91c4b00c4cbbbc5
Aleksandr Savchenkov <deathinfront@gmail.com>
2015-12-26 17:02:09 -0800

    first commit

---
+++ rigidbodyTest/Assets/custom scripts/SimpleCarController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SimpleCarController : MonoBehaviour
+{
+    public SideInfo leftSideInfo;
+    public SideInfo rightSideInfo;
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+
+
+
+    public void FixedUpdate()
+    {
+       // float throttle= Input.GetAxis("Vertical");
+       // float steering = Input.GetAxis("Horizontal");
+        
+        float leftTorque = Input.GetAxis("leftTank") *maxMotorTorque;
+        float rightTorque = Input.GetAxis("rightTank") * maxMotorTorque;
+
+        //print(leftTorque + "   " + rightTorque);
+        print(leftSideInfo.frontWheel.rpm + "   " + rightSideInfo.frontWheel.rpm);
+
+        leftSideInfo.setTorque(leftTorque);
+        rightSideInfo.setTorque(rightTorque);
+        
+    }
+}
+
+[System.Serializable]
+public class SideInfo
+{
+    public WheelCollider frontWheel;
+    public WheelCollider midWheel;
+    public WheelCollider rearWheel;
+    
+    public bool chained; // are these wheels chained together?
+
+    public void setTorque(float torque)
+    {
+        frontWheel.motorTorque = torque;
+        //midWheel.motorTorque = torque;
+        rearWheel.motorTorque = torque;
+    }
+}
---
+++ rigidbodyTest/Assets/custom scripts/customWheelCollider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class customWheelCollider : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    void OnCollisionEnter (Collision col){
+        print("enter called  "+ this.gameObject.name + ":   " + col.impactForceSum + "   " + col.impulse + "   " + col.frictionForceSum);
+    }
+    void OnCollisionStay(Collision col){
+        print(this.gameObject.name + ":   " + col.impactForceSum + "   " + col.impulse + "   " + col.frictionForceSum);
+    }
+}
