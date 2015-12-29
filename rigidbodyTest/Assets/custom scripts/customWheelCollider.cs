af16757fc18d9c491dcb359b32cf834a98cf34f2
Aleksandr Savchenkov <deathinfront@gmail.com>
2015-12-29 00:53:41 -0800

    finished heirarchy change for combined wheels. Now a list inside a dictionary.

--- rigidbodyTest/Assets/custom scripts/customWheelCollider.cs
+++ rigidbodyTest/Assets/custom scripts/customWheelCollider.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 public class customWheelCollider : MonoBehaviour
 { 
 
-    Dictionary<string, Collision> collisionDictionary = new Dictionary<string, Collision>();
+    Dictionary<string, List<Collision>> collisionDictionary = new Dictionary<string, List<Collision>>();
     Rigidbody parentRB;
 
     void Start()
@@ -13,32 +13,43 @@ public class customWheelCollider : MonoBehaviour
     }
     void FixedUpdate()
     {
+
+        //col.contacts[0].thisCollider.GetComponentInChildren<wheelInfo>().COF); // this is the correct way to retrieve wheel info
+
         float leftTorque = Input.GetAxis("leftTank") * 20;
         float rightTorque = Input.GetAxis("rightTank") * 20;//get the supposed torques we are meant to apply to each side
 
-        foreach (KeyValuePair<string,Collision> colPair in collisionDictionary)
+        foreach (KeyValuePair<string, List<Collision>> chainedWheels in collisionDictionary)
         {
-            Collision col = colPair.Value;
+            List<Collision> wheels = chainedWheels.Value;
+            print(wheels.Count);
             //print(col.contacts[0].thisCollider.name);
         }
+        //print(parentRB.velocity + "   " + parentRB.GetP);
+        collisionDictionary.Clear();
     }
 
     void OnCollisionEnter(Collision col)
     {
-        if (col.contacts[0].thisCollider.transform.parent.parent.name == "wheels") collisionDictionary[col.contacts[0].thisCollider.name] = col;
+        saveCollision(col);
     }
     void OnCollisionStay(Collision col)
     {
-        if(col.contacts[0].thisCollider.transform.parent.parent.name=="wheels") collisionDictionary[col.contacts[0].thisCollider.name] = col;
+        saveCollision(col);
     }
 
     void saveCollision(Collision col)
     {
-        //col.contacts[0].thisCollider.GetComponentInChildren<wheelInfo>().COF); // this is the correct way to retrieve wheel info
-
-        
-
-        //print(parentRB.velocity + "   " + parentRB.GetP);
-
+        if (col.contacts[0].thisCollider.transform.parent.parent.name == "wheels")
+        {
+            if (collisionDictionary.ContainsKey(col.contacts[0].thisCollider.transform.parent.name))
+            {
+                collisionDictionary[col.contacts[0].thisCollider.transform.parent.name].Add(col);
+            }
+            else
+            {
+                collisionDictionary.Add(col.contacts[0].thisCollider.transform.parent.name, new List<Collision> { col });
+            }
+        }
     }
 }
