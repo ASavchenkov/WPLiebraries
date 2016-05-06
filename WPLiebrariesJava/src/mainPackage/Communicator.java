8990bc17b0e77ab0eb5460deffb142c3705fede2
Sasha <asavchenkov1@gmail.com>
2016-05-05 23:18:00 -0700

    major changes to the communication framework.
    json now goes through gson. sensor values stored in a hashmap.
    
    transitioning to fullserializer for the c# part.

--- WPLiebrariesJava/src/mainPackage/Communicator.java
+++ WPLiebrariesJava/src/mainPackage/Communicator.java
@@ -4,49 +4,70 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.google.gson.Gson;
 
 /*
  * @author Aleksandr Savchenkov
  */
-public class Communicator {
+public class Communicator implements Runnable{
 
+	InetAddress simAddr; //dont initialize this because you cant
+	DatagramSocket sock; //initialize this in constructor with correct port.
 	
-	public Communicator(int ownPort, int simPort){
-		try {
-			
-			DatagramSocket sock = new DatagramSocket(3476);
-			
-			byte[] sendData = new byte[1024];
-			byte[] recieveData = new byte[1024];
-			
-			String msg = "YO WASSSUP MOFO";
-			
-			InetAddress simAddr = InetAddress.getLoopbackAddress();
-			
-			sendData = msg.getBytes();
-			
-			while(true){
-				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length,simAddr,696);   
+	byte[] sendBuf = new byte[1024];
+	byte[] rcvBuf = new byte[1024];
+	DatagramPacket rcvPacket = new DatagramPacket(rcvBuf, rcvBuf.length);
+	
+	int ownPort = -1;
+	int simPort = -1;
+	
+	Gson gson = new Gson(); //use this to actually convert shit.
+	
+	Map rcvDataMap = new HashMap();
+	//use this map to store all most current data from the sim.
+	
+	
+	/*
+	 * @param _ownPort is the port this app will send data from.
+	 */
+	public Communicator( int _simPort) throws IOException{
+		
+		simPort = _simPort;
+		simAddr = InetAddress.getLoopbackAddress();
+		sock = new DatagramSocket();
+		ownPort = sock.getLocalPort();//we never really use this.
+		
+		
+	}
+	
+	public void run(){
+		while(true){
+			try {
+				sock.receive(rcvPacket); // blocks the thread till we get some packet.
+				String rcvMsg = new String(rcvPacket.getData(),0,rcvPacket.getLength());//turns packet into string.
+				rcvDataMap.putAll(gson.fromJson(rcvMsg, HashMap.class)); // put the data we got into the sensorData
 				
-				sock.send(sendPacket);
-				
-				DatagramPacket rcvPacket = new DatagramPacket(recieveData, recieveData.length);
-				
-				sock.receive(rcvPacket);
-				String response = new String(rcvPacket.getData());
-				System.out.println(response);
-
-				System.out.println();
-			}			
-			
-			
-		} catch (SocketException e) {
-			e.printStackTrace();
-		} catch (IOException e){
-			e.printStackTrace();
+			} catch (IOException e) {
+				System.out.println("having trouble recieving data yo.");
+				e.printStackTrace();
+			}
 		}
 	}
 	
+	public void sendString(String msg) throws IOException{
+		sendBuf = msg.getBytes();
+		DatagramPacket sendPack = new DatagramPacket(sendBuf,sendBuf.length,simAddr,simPort);
+		sock.send(sendPack);
+	}
+	
+	public int getPort(){
+		return ownPort;
+	}
+	
+	public Object getValue(Object key){
+		return rcvDataMap.get(key);
+	}
 }
--- WPLiebrariesJava/src/mainPackage/IterativeRobot.java
+++ WPLiebrariesJava/src/mainPackage/IterativeRobot.java
@@ -1,15 +1,83 @@
 package mainPackage;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
 
 public class IterativeRobot {
 	
-	public static void main(String[] args) {
-		
-		
-		
-		
+	private enum RobotState{
+		DISABLED,
+		ROBOT_INIT,
+		AUTONOMOUS_INIT,
+		AUTONOMOUS_PERIODIC,
+		TELEOP_INIT,
+		TELEOP_PERIODIC
+	}
 	
+	static Communicator com;
+	static Thread comThread;
+	
+	static Gson gson = new Gson();
+	
+	public static void main(String[] args) {
+		try {
+			System.out.println("Attempting to take port: ");
+			com = new Communicator(696);
+		
+			comThread = new Thread(com);
+			comThread.start();
+			
+			RobotState curState = RobotState.DISABLED;
+			
+			Map robotHeader = new HashMap();
+			robotHeader.put("variant", "WPLiebraries");
+			robotHeader.put("port",com.getPort());
+
+			String headerString = gson.toJson(robotHeader);
+			
+			
+			while(true){
+				
+				/*
+				 * regardless of state we want to keep updating the 
+				 * header and sending it to the bot every 20 or so ms?
+				 * (50 if theres actual important info in there)	
+				 */
+				
+				try {
+					com.sendString(headerString);
+					System.out.println(com.getValue("port"));
+				} catch (IOException e1) {
+					System.out.println("yo having trouble communicating");
+					e1.printStackTrace();
+				}
+				
+				
+				
+				switch(curState){
+				case DISABLED:
+					break;
+				
+				}
+				
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+			}
+		
+		} catch (IOException e) {
+			System.out.println("failed to create Communicator");
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public static void robotInit(){
